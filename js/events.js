8a8528dba55761a7216620fc1d6971e51bffd6fc
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-04-04 08:14:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ function submitIt(){
   $('form').on('submit' ,function (){
     alert('Your form is going to be sumbitted now.')
     })
-  $('form').trigger('submit')
+  //$('form').trigger('submit')
 }
 
 $(document).ready(function(){
