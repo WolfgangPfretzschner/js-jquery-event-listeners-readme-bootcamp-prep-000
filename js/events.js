56a65d24b96c7f028c60b3f560cd3f96c304a6da
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-04-04 06:49:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
-//define functions here
+function getIt(){
+  $('div')on('click', function(){
+    alert ('Hey!')
+  })
+}
+
+function frameIt(){
+  
+}
+
+function pressIt(){
+  
+}
+
+function submitIt(){
+  
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
